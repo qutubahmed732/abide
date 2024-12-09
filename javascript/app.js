462fec87566cf775b27655d2b1ade7b9e73d5e5b
qutub ahmed <qutubahmed104@gmail.com>
2024-12-09 17:57:44 +0500

    last commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,10 @@
 const menu_button = document.querySelector("#menu_button");
 menu_button.addEventListener("click", ()=>{
-    const navbar_items = document.querySelector("#navbar_items");
-    
+    const navbar_items = document.querySelector("#navbar_items")
+
+    if(navbar_items.style.display == "block"){
+        navbar_items.style.display = "none";
+    } else {
+        navbar_items.style.display = "block";
+    }
 })
