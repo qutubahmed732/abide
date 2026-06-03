88bd45ba32a94623ac2280103fcf62b34d2eda54
Qutub Ahmed <qutubahmedkhan@outlook.com>
2026-06-03 23:48:41 +0500

    remove un-necessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,6 @@ export default function Home() {
                   fill
                   placeholder="blur"
                   sizes="(max-width: 768px) 100vw, 50vw"
-                  priority
                 />
               </div>
             </div>
